0c931f4fd543028abfc092c6e158fbd1694e3193
Zak <zakariyya.s.uddin@gmail.com>
2019-01-04 11:09:36 +0000

    Emite newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,8 @@
 let socket = io();
 
-// socket.on('connect', () => {
-//     console.log('Connected to server');
-// });
-
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Zak',
-        text: 'Yeah that is fine.'
-    });
 });
-
-// socket.on('disconnect', () => {
-//     console.log('Disconnected from server');
-// });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
